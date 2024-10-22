6471221a5838a6a47f59c9b5cd7de07496f9e8f1
erikfellipex <143715559+erikfellipex@users.noreply.github.com>
2024-10-22 10:29:52 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'RAP',
+    'Quem é o  Slim Shady?',
+    'eminem'
+)
+criaCartao(
+    'RAP',
+    ' Qual álbum do Racionais mais fez sucesso?',
+    'Sobrevivendo no inferno'
+)
+
+criaCartao(
+    'RAP',
+    'Qual é o nome da gravadora do snoop dogg?',
+    'Death Row Records',
+)
+
+criaCartao(
+    'RAP',
+    'Qual musica o eminem fez para a filha dele?',
+    'Mockingbird',
+)
+
+criaCartao(
+    'RAP',
+    'Qual o álbum mais famoso do Orochi?',
+    'Celebridade',
+)
+
+
+criaCartao(
+    'RAP',
+    'Qual o cantor de Rap mais icônico do Brasil?',
+    'Sabotagem',
+)
